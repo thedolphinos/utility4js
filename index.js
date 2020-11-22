48459ac76746d234ef6d4661b18076307cf9aded
Onur Çipe <onur@dolphinos.com>
2020-11-23 02:31:09 +0300

    feature: export errors

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const isObjectId = require("./lib/db/isObjectId");
 
 const init = require("./lib/variable/init");
 
+const Error = require("@thedolphinos/error4js"); // this is exported due to node's duplicate module imports which causes reference problems during instanceof checks.
+
 module.exports = {
   isExist,
   isInitialized,
@@ -31,5 +33,7 @@ module.exports = {
 
   isObjectId,
 
-  init
+  init,
+
+  Error
 };
